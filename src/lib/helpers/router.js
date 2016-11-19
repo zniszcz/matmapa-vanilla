00ff96785d2b688f19b2eee095903e497835b4fe
Adrian 'Zniszczo' Zniszczyński <adrian@zniszczynski.pl>
2016-11-19 17:58:19 +0100

    Router incorrect hash handling

--- src/lib/helpers/router.js
+++ src/lib/helpers/router.js
@@ -1,17 +1,21 @@
 (function () {
   'use strict';
 
-  app.Router = class Router extends app.Abstract.Observable{
+  app.Router = class Router extends app.Abstract.Observable {
     constructor() {
       super();
       this.routes = {};
-      this.setDefaultRoute(() => console.log('DuUDUuUUpa'));
+      window.addEventListener('DOMContentLoaded', () => this.reload());
       window.addEventListener('hashchange', () => this.reload());
     }
     reload() {
       const params = this.parseHash();
-      this.setRouteParams(params);
-      this.execRoute(params.route);
+      if (params) {
+        this.setRouteParams(params.params);
+        this.execRoute(params.route);
+      } else {
+        this.backToDefaultRoute();
+      }
     }
     addRoute(hash, action) {
       this.routes[hash] = action;
@@ -20,66 +24,40 @@
     setDefaultRoute(href) {
       this.defaultRoute = href;
     }
-    otherwise(adres){
-      /*  -- Nie ta odpowiedzialność - dobry kod, złe miejsce
-      const doesntExist = false; // TODO
-      const isntSet = (!window.location.hash ||
-                       window.location.hash.length < 2 ||
-                       window.location.hash === '#/');
-
-      if(doesntExist || isntSet) {
-        window.location.hash = this.defaultRoute;
-      } else {
-      */
-        this.setDefaultRoute(adres);
-      //}
+    otherwise(href) {
+      this.setDefaultRoute(href);
+      return this;
     }
-    /* setCurrentView(View) {
-      // tutaj powinna być metoda czyszcząca wrapper i przypisująca nowy widok na wrapper
-      this.currentView = new View();
-    } */
-    setRouteParams(params = this.checkRouteParam()) {
+    setRouteParams(params = this.parseHash()) {
       this.routeParams = params;
     }
-
-    /* getCurrentView() {
-      return this.currentView;
-    } */
     getRouteParams() {
       return this.routeParams;
     }
-
     existRoute(href) {
       return (href in this.routes);
     }
 
     backToDefaultRoute() {
-      window.location.hash = '#'+this.defaultRoute;
+      window.location.hash = `#${this.defaultRoute}`;
     }
-
     execRoute(href) {
-      console.log("I executed " + href);
-      if (this.existRoute(href)) {
-        this.routes[href]();
-      } else {
-        this.backToDefaultRoute();
-      }
-    }
-
-    render() {
-      this.getCurrentView().render();
+      this.routes[href]();
     }
     parseHash() {
 
       const parsedHash = window.location.hash.split('/');
-      const params = {
-        route: parsedHash[1],
-        params: {
-          id: '1',
-        },
-        action: null,
-      };
-      return params;
+      const result = {};
+      const isCorrectHash = (parsedHash[0] === '#');
+
+      result.route = parsedHash[1] || '';
+      result.params = parsedHash.slice(2);
+
+      if (!this.existRoute(result.route) || !isCorrectHash) {
+        return null;
+      }
+
+      return result;
     }
-  }
+  };
 })();
